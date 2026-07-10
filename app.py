4c485c21d58ccd13cb5441d2267a27061ecc11f3
anup-codes <anup2812007@gmail.com>
2026-07-10 22:36:04 +0530

    using clean data and working on that

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-df= pd.read_csv('startup_funding.csv')
+df= pd.read_csv('F:\Pythonn\startup-dashboard\startup_cleaned.csv')
 #data cleaning...............
-df['Investors Name']=df['Investors Name'].fillna('Undisclosed')
+# df['Investors Name']=df['Investors Name'].fillna('Undisclosed')
 
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One', ['Overall Analysis','StartUp', 'Investor'])
@@ -12,11 +12,13 @@ option = st.sidebar.selectbox('Select One', ['Overall Analysis','StartUp', 'Inve
 if option == 'Overall Analysis':
   st.title('Overall Analysis')
 elif option == 'StartUp':
-  st.sidebar.selectbox('Select startup',sorted(df['Startup Name'].unique()))
+  st.sidebar.selectbox('Select startup',sorted(df['startup'].unique()))
   btn1 = st.sidebar.button('Find Startup Details')
   st.title('StartUp Analysis')
 
 else:
-  st.sidebar.selectbox('Select startup',sorted(df['Investors Name'].unique()))
+  st.sidebar.selectbox('Select startup',sorted(set(df['investors'].str.split(',').sum())))
   btn2 = st.sidebar.button('Find Investor Details')
   st.title('Investor Analysis')
+
+
