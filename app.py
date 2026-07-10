2cead605443e36db21c17a7c1f428b5ab65a0b26
anup-codes <anup2812007@gmail.com>
2026-07-10 23:47:16 +0530

    biggest investment of the investors

--- app.py
+++ app.py
@@ -5,6 +5,24 @@ df= pd.read_csv('F:\Pythonn\startup-dashboard\startup_cleaned.csv')
 #data cleaning...............
 # df['Investors Name']=df['Investors Name'].fillna('Undisclosed')
 
+def load_investor_details(investor):
+ 
+  st.title(investor)
+
+  #load the recent 5 investment of the investor
+  last5_df = df[df['investors'].str.contains(investor)].head()[['date','startup','vertical','city','round','amount']]
+  st.subheader('Most Recent Investments')
+  st.dataframe(last5_df)
+
+
+  #biggest investments
+  big_df = df[df['investors'].str.contains(investor)].groupby('startup')['amount'].sum().sort_values(ascending = False).head()
+  st.subheader('Biggest Investments')
+  st.dataframe(big_df)
+
+
+
+
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One', ['Overall Analysis','StartUp', 'Investor'])
 
@@ -17,8 +35,11 @@ elif option == 'StartUp':
   st.title('StartUp Analysis')
 
 else:
-  st.sidebar.selectbox('Select startup',sorted(set(df['investors'].str.split(',').sum())))
+  selected_investor = st.sidebar.selectbox('Select startup',sorted(set(df['investors'].str.split(',').sum())))
   btn2 = st.sidebar.button('Find Investor Details')
-  st.title('Investor Analysis')
+  
+  if btn2:
+    load_investor_details(selected_investor)
+  
 
 
